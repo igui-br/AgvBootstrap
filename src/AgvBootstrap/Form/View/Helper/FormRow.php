5f863b19755370e769f37ae89c508c5313b0a1e9
vincequeiroz <victorguedes@gmail.com>
2014-02-25 19:40:35 -0300

    Update helper formRow

--- src/AgvBootstrap/Form/View/Helper/FormRow.php
+++ src/AgvBootstrap/Form/View/Helper/FormRow.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace AgvBootstrap\Form\View\Helper;
 
 use Zend\Form\View\Helper\FormRow as ZendFormRow;
@@ -19,52 +18,53 @@ class FormRow extends ZendFormRow
 
     /**
      * The class that is added to element that have errors
+     *
      * @var string
      */
     protected $inputErrorClass = 'has-error';
 
     /**
      * Form Type such as inline and horizontal
+     *
      * @var string
      */
     protected $formType = null;
 
     /**
      * Row class
-     * @var string 
+     *
+     * @var string
      */
     protected $rowClass = 'form-group';
-    
+
     /**
      * Element class
-     * @var string 
+     *
+     * @var string
      */
     protected $elementClass = 'form-control';
 
     /**
      * Invoke helper as functor
      *
-     * @param  null|ElementInterface $element
-     * @param  null|string           $formType
-     * @param  null|string           $labelPosition
-     * @param  bool                  $renderErrors
-     * @param  string|null           $partial
-     * @return string|FormRow
+     * @param null|ElementInterface $element
+     * @param null|string $formType
+     * @param null|string $labelPosition
+     * @param bool $renderErrors
+     * @param string|null $partial
+     * @return string FormRow
      */
-    public function __invoke(ElementInterface $element = null, $formType = null,
-                             $labelPosition = null, $renderErrors = null,
-                             $partial = null)
+    public function __invoke(ElementInterface $element = null, $formType = null, $labelPosition = null, $renderErrors = null, $partial = null)
     {
         $this->formType = $formType;
 
-        return parent::__invoke($element, $labelPosition, $renderErrors,
-                $partial);
+        return parent::__invoke($element, $labelPosition, $renderErrors, $partial);
     }
 
     /**
      * Utility form helper that renders a label (if it exists), an element and errors
      *
-     * @param  ElementInterface $element
+     * @param ElementInterface $element
      * @throws \Zend\Form\Exception\DomainException
      * @return string
      */
@@ -80,9 +80,7 @@ class FormRow extends ZendFormRow
         if (isset($label) && '' !== $label) {
             // Translate the label
             if (null !== ($translator = $this->getTranslator())) {
-                $label = $translator->translate(
-                    $label, $this->getTranslatorTextDomain()
-                );
+                $label = $translator->translate($label, $this->getTranslatorTextDomain());
             }
         }
 
@@ -92,7 +90,7 @@ class FormRow extends ZendFormRow
                 'label' => $label,
                 'labelAttributes' => $this->labelAttributes,
                 'labelPosition' => $this->labelPosition,
-                'renderErrors' => $this->renderErrors,
+                'renderErrors' => $this->renderErrors
             );
 
             return $this->view->render($this->partial, $vars);
@@ -103,74 +101,58 @@ class FormRow extends ZendFormRow
             $elementErrors = $elementErrorsHelper->render($element);
         }
 
-        if (!$element->hasAttribute('id')) {
+        if (! $element->hasAttribute('id')) {
             $element->setAttribute('id', $this->getId($element));
         }
 
         if (isset($label) && '' !== $label) {
             $label = $escapeHtmlHelper($label);
         }
-        
+
         if ($element instanceof Radio) {
             $rowClass = $this->rowClass;
 
-            $markup = sprintf(
-                '<fieldset><legend>%s</legend>%s</fieldset>', $label,
-                $elementHelper->render($element)
-            );
-            
+            $markup = sprintf('<fieldset><legend>%s</legend>%s</fieldset>', $label, $elementHelper->render($element));
+
             $markup = $this->getDivRadioCheckboxHorizontal($markup);
         } elseif ($element instanceof MultiCheckbox) {
             $rowClass = $this->rowClass;
-            $element->setLabelAttributes(array('class' => 'checkbox'));
+            $element->setLabelAttributes(array(
+                'class' => 'checkbox'
+            ));
 
-            $markup = sprintf(
-                '<fieldset><legend>%s</legend>%s</fieldset>', $label,
-                $elementHelper->render($element)
-            );
+            $markup = sprintf('<fieldset><legend>%s</legend>%s</fieldset>', $label, $elementHelper->render($element));
 
             $markup = $this->getDivRadioCheckboxHorizontal($markup);
         } elseif ($element instanceof Checkbox) {
             $rowClass = 'checkbox';
 
-            $markup = $labelHelper->openTag($this->getLabelAttributesByElement($element))
-                . $elementHelper->render($element)
-                . $label
-                . $labelHelper->closeTag();
+            $markup = $labelHelper->openTag($this->getLabelAttributesByElement($element)) . $elementHelper->render($element) . $label . $labelHelper->closeTag();
         } elseif ($element instanceof \Zend\Form\Element\File) {
             $rowClass = $this->rowClass;
-            $label = $labelHelper->openTag($this->getLabelAttributesByElement($element))
-                . $label
-                . $labelHelper->closeTag();
+            $label = $labelHelper->openTag($this->getLabelAttributesByElement($element)) . $label . $labelHelper->closeTag();
             $markup = $label . $elementHelper->render($element);
         } else if ($element instanceof Hidden) {
             return $elementHelper->render($element);
         } else if ($element instanceof Select) {
             $rowClass = $this->rowClass;
-
             $element->setAttribute('class', $this->elementClass);
-
-            $label = $labelHelper->openTag($this->getLabelAttributesByElement($element))
-                . $label
-                . $labelHelper->closeTag();
-            if ($this->formType == AgvBootstrapForm::FORM_TYPE_INLINE) {
-                $label = '';
-            }
-
+            $label = $labelHelper->openTag($this->getLabelAttributesByElement($element)) . $label . $labelHelper->closeTag();
             $markup = $label . $elementHelper->render($element);
+        } else if ($element instanceof Submit || $element instanceof Button) {
+            $rowClass = $this->rowClass;
+            $markup = $elementHelper->render($element);
         } else {
             $rowClass = $this->rowClass;
 
             $elementClass = $element->getAttribute('class');
-            if (!empty($elementClass)) {
+            if (! empty($elementClass)) {
                 $elementClass .= ' ';
             }
             $elementClass .= $this->elementClass;
             $element->setAttribute('class', $elementClass);
 
-            $label = $labelHelper->openTag($this->getLabelAttributesByElement($element))
-                . $label
-                . $labelHelper->closeTag();
+            $label = $labelHelper->openTag($this->getLabelAttributesByElement($element)) . $label . $labelHelper->closeTag();
             $markup = $label . $elementHelper->render($element);
         }
 
@@ -180,17 +162,16 @@ class FormRow extends ZendFormRow
 
         $errorClass = $this->getInputErrorClass();
 
-        if (count($element->getMessages()) && !empty($errorClass)) {
+        if (count($element->getMessages()) && ! empty($errorClass)) {
             $rowClass .= ' ' . $errorClass;
         }
 
-        return sprintf(
-            '<div class="%s">%s</div> ', $rowClass, $markup
-        );
+        return sprintf('<div class="%s">%s</div> ', $rowClass, $markup);
     }
 
     /**
      * Return label element
+     *
      * @param ElementInterface $element
      * @return string
      */
@@ -202,29 +183,46 @@ class FormRow extends ZendFormRow
             $labelAttributes = array();
         }
 
-        if (!isset($labelAttributes['for'])) {
+        if (! isset($labelAttributes['for'])) {
             $labelAttributes['for'] = $element->getAttribute('id');
         }
 
-        if (isset($labelAttributes['class'])) {
-            $labelAttributes['class'] .= ' ';
-        } else {
-            $labelAttributes['class'] = '';
+        if ($this->formType == AgvBootstrapForm::FORM_TYPE_HORIZONTAL) {
+            $class = array();
+            if (isset($labelAttributes['class']))
+                $class = explode(' ', $labelAttributes['class']);
+
+            array_unshift($class, 'control-label');
+
+            if (isset($labelAttributes['grid'])) {
+                $size = $labelAttributes['grid'];
+
+                if (is_array($size)) {
+                    $class = array_merge($class, $size);
+                } elseif (is_int($size)) {
+                    $class[] = sprintf('col-lg-%s col-md-%s col-sm-%s col-xs-%s', $size, $size, $size, $size);
+                } else {
+                    $class[] = $size;
+                }
+            }
+
+            if (isset($labelAttributes['offset'])) {
+                $offset = $labelAttributes['offset'];
+
+                if (is_array($offset)) {
+                    $class = array_merge($class, $offset);
+                } elseif (is_int($offset)) {
+                    $class[] = sprintf('col-lg-offset-%s col-md-offset-%s col-sm-offset-%s col-xs-offset-%s', $offset, $offset, $offset, $offset);
+                } else {
+                    $class[] = $offset;
+                }
+            }
+
+            $labelAttributes['class'] = implode(' ', array_unique($class));
         }
 
-        if (!isset($labelAttributes['size'])) {
-            $size = $element->getOption('size') ? 2 : 0;
-        } else {
-            $size = $labelAttributes['size'] ? 2 : 0;
-        }
-
-        $labelAttributes['class'] .= 'control-label ';
-
-        if (!empty($size)) {
-            $labelAttributes['class'] .= sprintf(
-                ' col-lg-%s col-md-%s col-sm-%s col-xs-%s ', $size, $size,
-                $size, $size
-            );
+        if ($this->formType == AgvBootstrapForm::FORM_TYPE_INLINE) {
+            $labelAttributes['class'] = 'sr-only';
         }
 
         return $labelAttributes;
@@ -232,6 +230,7 @@ class FormRow extends ZendFormRow
 
     /**
      * Return div element for checkbox and radio in horizontal layout
+     *
      * @param string $markup
      * @return string
      */
@@ -239,12 +238,9 @@ class FormRow extends ZendFormRow
     {
         if ($this->formType == AgvBootstrapForm::FORM_TYPE_HORIZONTAL) {
             $class = 'col-sm-offset-2 col-sm-10';
-            $markup = sprintf(
-                '<div class="%s">%s</div> ', $class, $markup
-            );
+            $markup = sprintf('<div class="%s">%s</div> ', $class, $markup);
         }
 
         return $markup;
     }
-
 }
