ee1e91ac9f6f1c2cd220e54ce18a16a41e4b67a3
vincequeiroz <victorguedes@gmail.com>
2014-02-25 16:11:23 -0300

    Update helper formSubmit

--- src/AgvBootstrap/Form/View/Helper/FormSubmit.php
+++ src/AgvBootstrap/Form/View/Helper/FormSubmit.php
@@ -1,51 +1,53 @@
 <?php
-
 namespace AgvBootstrap\Form\View\Helper;
 
 use Zend\Form\View\Helper\FormSubmit as ZendFormSubmit;
 use Zend\Form\ElementInterface;
-use Zend\Form\Exception;
 
 class FormSubmit extends ZendFormSubmit
 {
 
     /**
-     * Invoke helper as functor
+     * Render a form <input> element from the provided $element
      *
-     * Proxies to {@link render()}.
-     *
-     * @param  ElementInterface|null $element
-     * @return string|FormInput
+     * @param  ElementInterface $element
+     * @throws Exception\DomainException
+     * @return string
      */
-    public function __invoke(ElementInterface $element = null)
-    {
-        if (!$element) {
-            return $this;
-        }
-
-        return $this->render($element);
-    }
-
     public function render(ElementInterface $element)
     {
         $name = $element->getName();
         if ($name === null || $name === '') {
             throw new Exception\DomainException(sprintf(
-                    '%s requires that the element has an assigned name; none discovered',
-                    __METHOD__
+                '%s requires that the element has an assigned name; none discovered',
+                __METHOD__
             ));
         }
 
-        $attributes = $element->getAttributes();
-        $attributes['name'] = $name;
-        $attributes['type'] = $this->getType($element);
+        $attributes          = $element->getAttributes();
+        $attributes['name']  = $name;
+        $attributes['type']  = $this->getType($element);
         $attributes['value'] = $element->getValue();
-        
+
+        $this->getClass($attributes);
+
         return sprintf(
-                '<input class="btn btn-primary" %s%s',
-                $this->createAttributesString($attributes),
-                $this->getInlineClosingBracket()
+            '<input %s%s',
+            $this->createAttributesString($attributes),
+            $this->getInlineClosingBracket()
         );
     }
 
+    public function getClass(&$attributes)
+    {
+        $class = (key_exists('class', $attributes) ? $attributes['class'] : null);
+        if (!empty($class)) {
+            $class = explode(' ', $class);
+            array_unshift($class, 'btn', 'btn-default');
+
+            $attributes['class'] = implode(' ', array_unique($class));
+        } else {
+            $attributes['class'] = 'btn btn-default';
+        }
+    }
 }
